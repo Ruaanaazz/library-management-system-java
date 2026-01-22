18ba75147e5f8c42c50d901406b9fbc845991d8d
RUAANAAZ <ruaanaazmohd@gmail.com>
2026-01-23 00:39:45 +0530

    clarify db credential handling

--- src/dbutils/DBConnection.java
+++ src/dbutils/DBConnection.java
@@ -1,42 +1,24 @@
 package dbutils;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Properties;
-import java.io.InputStream;
-
 public class DBConnection {
 
-    public static Connection getConnection() {
-        Connection conn = null;
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("connection ok");
+			String url = "jdbc:oracle:thin:@localhost:1521:XE";
+            String user = "apex4";
+            String pass = "apex1234";
+           conn = DriverManager.getConnection(url, user, pass);
+           System.out.println("connection ok");
+					
+		}catch(Exception e) {
+			System.out.println("error connection"+e);
+		}
 
-        try {
-            Properties props = new Properties();
+return conn;
+	}
 
-            InputStream input = DBConnection.class
-                    .getClassLoader()
-                    .getResourceAsStream("config.properties");
-
-            if (input == null) {
-                System.out.println("Sorry, unable to find config.properties");
-                return null;
-            }
-
-            props.load(input);
-
-            String url = props.getProperty("db.url");
-            String user = props.getProperty("db.username");
-            String pass = props.getProperty("db.password");
-
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            conn = DriverManager.getConnection(url, user, pass);
-
-            System.out.println("Database connected successfully");
-
-        } catch (Exception e) {
-            System.out.println("DB Connection Error: " + e.getMessage());
-        }
-
-        return conn;
-    }
 }
